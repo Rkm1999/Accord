5af0798ed756f13e6f594fb40b5be5640e3d8453
Rkm1999 <junida1999@gmail.com>
2026-02-02 13:08:28 -0800

    removed dulplicated Item

--- database/migrations/0001_init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS messages (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    message TEXT NOT NULL,
-    timestamp INTEGER NOT NULL
-);
-
-CREATE INDEX IF NOT EXISTS idx_timestamp ON messages(timestamp);
